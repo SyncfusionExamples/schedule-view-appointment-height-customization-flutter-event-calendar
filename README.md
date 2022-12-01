63e114594e650438f9131b9062611bc58acdcb71
Muniappan <muniappan.subramanian@syncfusiopn.com>
2022-12-01 16:46:44 +0530

    sample updated

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-**[View document in our Syncfusion Flutter knowledge base](https://www.syncfusion.com/kb/12226/how-to-customize-the-appointment-height-in-schedule-view-of-the-flutter-event-calendar)**
+# appointment_height_customization
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,89 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-void main() => runApp(ScheduleView_AppointmentHeight());
+void main() {
+  runApp(const MyApp());
+}
 
-class ScheduleView_AppointmentHeight extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            body: SafeArea(
-                child: SfCalendar(
-          view: CalendarView.schedule,
-          dataSource: getCalendarDataSource(),
-          scheduleViewSettings: ScheduleViewSettings(
-              appointmentItemHeight: 70, hideEmptyScheduleWeek: true),
-        ))));
+      title: 'AppointmentHeight Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'AppointmentHeight Demo Home Page'),
+    );
   }
+}
 
-  _DataSource getCalendarDataSource() {
-    final List<Appointment> appointments = <Appointment>[];
-    appointments.add(Appointment(
-      startTime: DateTime(2020,12,1,9,0,0),
-      endTime: DateTime(2020,12,1,10,0,0),
-      subject: 'Release Meeting',
-      color: Colors.lightBlueAccent,
-    ));
-    appointments.add(Appointment(
-      startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
-      endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
-      subject: 'Release Meeting',
-      color: Colors.lightBlueAccent,
-    ));
-    appointments.add(Appointment(
-      startTime: DateTime.now().add(const Duration(hours: 2, days: -2)),
-      endTime: DateTime.now().add(const Duration(hours: 4, days: -2)),
-      subject: 'Performance check',
-      color: Colors.amber,
-    ));
-    appointments.add(Appointment(
-      startTime: DateTime.now().add(const Duration(hours: 6, days: -3)),
-      endTime: DateTime.now().add(const Duration(hours: 7, days: -3)),
-      subject: 'Support',
-      color: Colors.green,
-    ));
-    appointments.add(Appointment(
-      startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
-      endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
-      subject: 'Retrospective',
-      color: Colors.purple,
-    ));
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
 
-    return _DataSource(appointments);
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+        body: SafeArea(
+            child: SfCalendar(
+              view: CalendarView.schedule,
+              dataSource: getCalendarDataSource(),
+              scheduleViewSettings: const ScheduleViewSettings(
+                  appointmentItemHeight: 70, hideEmptyScheduleWeek: true),
+            )) // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
+}
+
+_DataSource getCalendarDataSource() {
+  final List<Appointment> appointments = <Appointment>[];
+  appointments.add(Appointment(
+    startTime: DateTime(2020,12,1,9,0,0),
+    endTime: DateTime(2020,12,1,10,0,0),
+    subject: 'Release Meeting',
+    color: Colors.lightBlueAccent,
+  ));
+  appointments.add(Appointment(
+    startTime: DateTime.now().add(const Duration(hours: 4, days: -1)),
+    endTime: DateTime.now().add(const Duration(hours: 5, days: -1)),
+    subject: 'Release Meeting',
+    color: Colors.lightBlueAccent,
+  ));
+  appointments.add(Appointment(
+    startTime: DateTime.now().add(const Duration(hours: 2, days: -2)),
+    endTime: DateTime.now().add(const Duration(hours: 4, days: -2)),
+    subject: 'Performance check',
+    color: Colors.amber,
+  ));
+  appointments.add(Appointment(
+    startTime: DateTime.now().add(const Duration(hours: 6, days: -3)),
+    endTime: DateTime.now().add(const Duration(hours: 7, days: -3)),
+    subject: 'Support',
+    color: Colors.green,
+  ));
+  appointments.add(Appointment(
+    startTime: DateTime.now().add(const Duration(hours: 6, days: 2)),
+    endTime: DateTime.now().add(const Duration(hours: 7, days: 2)),
+    subject: 'Retrospective',
+    color: Colors.purple,
+  ));
+
+  return _DataSource(appointments);
 }
 
 class _DataSource extends CalendarDataSource {
